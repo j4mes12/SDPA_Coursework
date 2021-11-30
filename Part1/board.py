60a6101c97ae3d2fcfcc51ca337770e57588ea3e
j4mes12 <jamesstephenson@Jamess-Air.cust.communityfibre.co.uk>
2021-11-30 08:51:14 +0000

    Changes to board and main in part1. Need to make sure python3 is being used instead of python2

--- Part1/board.py
+++ Part1/board.py
@@ -1,11 +1,26 @@
+import numpy as np
+
+
 class Board:
-    def __init__(self, p1_position, p2_position, board_size):
+    def __init__(self, p1_position, p2_position, n):
         self.p1_position = p1_position
         self.p2_position = p2_position
-        self.board_size = board_size
+        self.n = n
 
-    def print_board(self, board_size):
-        print("board")
+    def print_board(self, n):
+        self.board = np.zeros(shape=(n, n)).astype(int)
+        self.board[0, 0] = "1"
+        self.board[-1, -1] = "2"
+
+        for i in range(n):
+            print("#|", end="")
+            for j in range(n):
+                if self.board[i, j] == 0:
+                    out = " "
+                else:
+                    out = self.board[i, j]
+                print(f"{out}|", end="")
+            print("#")
 
     def execute_move(self, player, move):
         print("player has moved")
--- Part1/main.py
+++ Part1/main.py
@@ -1,16 +1,18 @@
-import player as p
+# import player as p
 import board as b
 
-board_size = input("Welcome to the game. Please enter board size: ")
+game_board = b.Board()
 
-b.print_board()
+# board_size = input("Welcome to the game. Please enter board size: ")
 
-play = True
-while play:
-    p1_move = input("Enter move of player 1: ")
-    p.update_position(p1_move)
-    b.execute_move("p1", p1_move)
+# game_board.print_board(board_size)
 
-    p2_move = input("Enter move of player 2: ")
-    b.execute_move("p2", p2_move)
-    p.update_position(p2_move)
+# play = True
+# while play:
+#     p1_move = input("Enter move of player 1: ")
+#     p.update_position(p1_move)
+#     b.execute_move("p1", p1_move)
+
+#     p2_move = input("Enter move of player 2: ")
+#     b.execute_move("p2", p2_move)
+#     p.update_position(p2_move)
