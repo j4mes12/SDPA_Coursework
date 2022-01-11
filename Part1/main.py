f75da893424500fb80d2fa4b86b5c95ab9a30178
j4mes12 <jamesstephenson@Jamess-Air.cust.communityfibre.co.uk>
2022-01-11 14:54:03 +0000

    added comments to main.py on new code

--- Part1/main.py
+++ Part1/main.py
@@ -16,6 +16,7 @@ def main():
     """This is the main function that is run and controls the program. It contains
     the menu procedure, board size request and executes the game."""
 
+    ######## CHOOSE GAME TYPE ########
     # This while loop requests the user's game-type choice and breaks once there is an acceptable entry
     while True:
         game_type = input(
@@ -27,7 +28,7 @@ def main():
             Choice: """
         )
         if game_type == "q":  # Provides an option to quit the game
-            return print("Game Ended.")
+            return print("Game Quitted.")
         if game_type == "":  # Provides user a random option
             game_type = random.sample("12", 1)[0]
             print(f"Option {game_type} Randomly Selected!")
@@ -41,6 +42,7 @@ def main():
     gt_dict = {"1": "basic", "2": "sim"}
     game_type = gt_dict[game_type]
 
+    ######## CHOOSE OPPONENT TYPE ########
     # This while loop requests the user's opponent choice and breaks once there is an acceptable entry
     global opponent_type
     while True:
@@ -54,7 +56,7 @@ def main():
             Choice: """
         )
         if opponent_type == "q":  # Provides an option to quit the game
-            return print("Game Ended.")
+            return print("Game Quitted.")
         if opponent_type == "":  # Provides user a random option
             opponent_type = random.sample("123", 1)[0]
             print(f"Option {opponent_type} Randomly Selected!")
@@ -68,13 +70,14 @@ def main():
     opponent_dict = {"1": "Human", "2": "RComp", "3": "SComp"}
     opponent_type = opponent_dict[opponent_type]
 
+    ######## GET BOARD SIZE ########
     # This while loop asks the user to enter a suitable board size
     while True:
         board_size = input("Please enter board size (3 or higher, Blank for Random!): ")
         if board_size == "q":  # Provides an option to quit the game
-            return print("Game Ended.")
-        elif board_size == "":
-            board_size = random.randint(3, 9)
+            return print("Game Quitted.")
+        elif board_size == "":  # Enact the random option
+            board_size = random.randint(3, 20)
             print(f"Random Board Size: {board_size}")
             break
         elif board_size.strip().isdigit():  # Makes sure input in digit
@@ -88,9 +91,11 @@ def main():
         else:
             print(f"Invalid Board Size. Input: {board_size}")
 
+    # Creates global variable game so it can be used in game type functions
+    global game
+
     # Creates the game instance
     # If versus computer options are chosen, player2 is a Computer
-    global game
     if opponent_type[1:] == "Comp":
         game = Board(board_size, computer=True)
     else:
@@ -101,12 +106,12 @@ def main():
 
     # The specific type of game is executed based on the menu choice
     if game_type == "basic":
-        new_basic_game()
+        basic_game()
     elif game_type == "sim":
-        new_simultaneous_game()
+        simultaneous_game()
 
 
-def new_basic_game():
+def basic_game():
     """This function contains the method to run the basic game whereby
     moves are inputted alternately and the board is displayed after each
     move. Uses global variables game and opponent_type to take into account
@@ -121,7 +126,7 @@ def new_basic_game():
 
         # Checks if input is valid
         if game.player1.in_value == "q":
-            return print("Game Ended.")
+            return print("Game Quitted.")
         elif game.player1.check_legal_move():
             game.player1.change_direction()
         else:
@@ -139,19 +144,21 @@ def new_basic_game():
         # Display board
         game.print_board()
 
+        # Check if we need to get second input
         if opponent_type == "Human":
             # Call get_input for player input to get player's move
             game.player2.get_input()
 
             # Checks if input is valid
             if game.player2.in_value == "q":
-                return print("Game Ended.")
+                return print("Game Quitted.")
             elif game.player2.check_legal_move():
                 game.player2.change_direction()
             else:
                 return game.player1.display_winner()
 
         else:
+            # If computer chosen, check which type of computer and get move
             if opponent_type == "RComp":
                 game.player2.gen_exe_computer_move(game, move_type="random")
             elif opponent_type == "SComp":
@@ -170,7 +177,7 @@ def new_basic_game():
         game.print_board()
 
 
-def new_simultaneous_game():
+def simultaneous_game():
     """This function contains the method to run the simultaneous game whereby moves
     are inputed simultaneously and the board is displayed after both moves have
     been executed. Uses global variables game and opponent_type to take into account
@@ -184,7 +191,7 @@ def new_simultaneous_game():
 
         # Check if input is to quit
         if game.player1.in_value == "q":
-            return print("Game Ended.")
+            return print("Game Quitted.")
 
         # Get input if opponent is human
         if opponent_type == "Human":
@@ -192,9 +199,10 @@ def new_simultaneous_game():
 
             # Check if input is to quit
             if game.player2.in_value == "q":
-                return print("Game Ended.")
+                return print("Game Quitted.")
 
         else:
+            # If a copmuter opponent is chosen, get moves accordingly
             if opponent_type == "RComp":
                 game.player2.gen_exe_computer_move(game, move_type="random")
             elif opponent_type == "SComp":
@@ -204,16 +212,15 @@ def new_simultaneous_game():
         lm_outcome = game.check_legal_moves_sim()
 
         # Make decisions based on lm_outcome return variable
-        if lm_outcome == "T0":
+        # Note: By definition, the computer's move will always be legal
+        if lm_outcome == "T0":  # Tie
             return print("Shake hands: It's a tie")
-        elif lm_outcome == "W1":
+        elif lm_outcome == "W1":  # Player 1 wins
             return game.player1.display_winner()
-        elif lm_outcome == "W2":
+        elif lm_outcome == "W2":  # Player 2 wins
             return game.player2.display_winner()
         else:
             game.player1.change_direction()
-
-            # By definition, the computer's move will always be legal
             if opponent_type == "Human":
                 game.player2.change_direction()
 
